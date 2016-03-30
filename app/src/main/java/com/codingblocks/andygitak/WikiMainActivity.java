ed4e0c750830a9c7fa29ff9d77293753d07632b8
raghav64 <raghavmehta64@gmail.com>
2016-03-30 13:56:58 +0530

    First Commit

---
+++ app/src/main/java/com/codingblocks/andygitak/WikiMainActivity.java
@@ -0,0 +1,13 @@
+package com.codingblocks.andygitak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WikiMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wiki_main);
+    }
+}
